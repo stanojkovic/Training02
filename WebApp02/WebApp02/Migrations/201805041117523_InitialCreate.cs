486f0904c7ff66610b05d02cec4fa14c0b85545c
Саша Станојковић <sasastanojkovic@hotmail.com>
2018-05-04 13:48:38 +0200

    I create dataBase and add some entery !

---
+++ WebApp02/WebApp02/Migrations/201805041117523_InitialCreate.cs
@@ -0,0 +1,28 @@
+namespace WebApp02.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        LastName = c.String(),
+                        BirthDate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
