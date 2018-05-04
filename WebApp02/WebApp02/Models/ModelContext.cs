5dd4d40802e6f9bd12cb565e664c8a26c369d873
Саша Станојковић <sasastanojkovic@hotmail.com>
2018-05-04 12:17:49 +0200

    I created new classes in Model folde !

---
+++ WebApp02/WebApp02/Models/ModelContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WebApp02.Models
+{
+    public class ModelContext : DbContext
+    {
+        public ModelContext() : base("ModelContext")        //ovaj string je naziv DataConnection
+        {
+
+        }
+
+        public virtual DbSet<User> Students { get; set; }
+    }
+}
